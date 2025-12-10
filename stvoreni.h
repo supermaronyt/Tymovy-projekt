7bcedfbe15a643644bb76438ca39c6bde48a3047
Ondřej Novák <novak.ondrej@sosasou.cz>
2025-12-10 10:27:52 +0100

    vytvoreni stvoreni
    hhehehehehehehehehehehehdehehhehehšehehehehehh

---
+++ stvoreni.h
@@ -0,0 +1,13 @@
+//
+// Created by novak.ondrej on 10.12.2025.
+//
+
+#ifndef TYMOVY_PROJEKT_STVORENI_H
+#define TYMOVY_PROJEKT_STVORENI_H
+
+
+class stvoreni {
+};
+
+
+#endif //TYMOVY_PROJEKT_STVORENI_H
