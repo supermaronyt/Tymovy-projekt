aa58c137d10b7d0676314c763d774a4082b6592e
Ondřej Novák <novak.ondrej@sosasou.cz>
2025-12-17 10:12:18 +0100

    Pridani class mapa

---
+++ Mapa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by novak.ondrej on 17.12.2025.
+//
+
+#include "Mapa.h"
---
+++ Mapa.h
@@ -0,0 +1,13 @@
+//
+// Created by novak.ondrej on 17.12.2025.
+//
+
+#ifndef TYMOVY_PROJEKT_MAPA_H
+#define TYMOVY_PROJEKT_MAPA_H
+
+
+class Mapa {
+};
+
+
+#endif //TYMOVY_PROJEKT_MAPA_H
